84c3ac17a12921dd8c7e7ac89bdb39b75bf31865
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-01-17 23:12:08 +0530

    contact page completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Services from './components/services/Services';
 import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <About />
       <Services />
       <Portfolio />
+      <Contact />
     </div>
   )
 }
